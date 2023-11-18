d0e6cddcfa0fa13c8d35ff8ad5b2d9e7f2dd4538
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-11-18 00:01:24 -0500

    Impl() - Create Constants and initializations.

--- app/src/main/java/com/cursosant/android/stores/mainModule/model/MainInteractor.kt
+++ app/src/main/java/com/cursosant/android/stores/mainModule/model/MainInteractor.kt
@@ -5,6 +5,7 @@ import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.cursosant.android.stores.StoreApplication
 import com.cursosant.android.stores.common.entities.StoreEntity
+import com.cursosant.android.stores.common.utils.Constants
 import com.google.gson.Gson
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
@@ -12,7 +13,7 @@ import org.jetbrains.anko.uiThread
 class MainInteractor {
 
     fun getStores(callback: (MutableList<StoreEntity>) -> Unit) {
-        val url = "https://store.free.beeceptor.com/my/api/path"
+        val url = Constants.STORES_URL + Constants.GET_ALL_PATH
 
         val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null, { response ->
             Log.i("Response", response.toString())
