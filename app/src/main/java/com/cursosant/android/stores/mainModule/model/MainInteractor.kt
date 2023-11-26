99a3470e97641b920660479ef45e43310b8bd7c8
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-11-25 20:50:23 -0500

    Impl() - Implement List with Gson and TypeToken

--- app/src/main/java/com/cursosant/android/stores/mainModule/model/MainInteractor.kt
+++ app/src/main/java/com/cursosant/android/stores/mainModule/model/MainInteractor.kt
@@ -7,6 +7,7 @@ import com.cursosant.android.stores.StoreApplication
 import com.cursosant.android.stores.common.entities.StoreEntity
 import com.cursosant.android.stores.common.utils.Constants
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
@@ -18,7 +19,18 @@ class MainInteractor {
         val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null, { response ->
             val status = response.getInt(Constants.STATUS_PROPERTY)
             if (status == Constants.SUCCESS) {
-                Log.i("status",status.toString())
+                Log.i("status", status.toString())
+                /*
+                val jsonObject = Gson().fromJson(
+                    response.getJSONArray(Constants.STORES_PROPERTY).get(0).toString(),
+                    StoreEntity::class.java
+                )
+                Log.i("storeEntity", jsonObject.toString())
+                */
+                val jsonList = response.getJSONArray(Constants.STORES_PROPERTY).toString()
+                val mutableListType = object : TypeToken<MutableList<StoreEntity>>() {}.type
+                val storeList = Gson().fromJson<MutableList<StoreEntity>>(jsonList, mutableListType)
+                callback(storeList)
             }
         }, {
             it.printStackTrace()
