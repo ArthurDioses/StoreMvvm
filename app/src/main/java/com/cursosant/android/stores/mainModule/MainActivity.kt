21204adc4d5e7c80dbff5bb30e58fbf3d60615ad
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-11-25 20:58:15 -0500

    Impl() - Implement ProgressBar for delay.

--- app/src/main/java/com/cursosant/android/stores/mainModule/MainActivity.kt
+++ app/src/main/java/com/cursosant/android/stores/mainModule/MainActivity.kt
@@ -3,6 +3,7 @@ package com.cursosant.android.stores.mainModule
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
@@ -43,6 +44,7 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         mMainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
         mMainViewModel.getStores().observe(this) { stores ->
             mAdapter.setStores(stores)
+            mBinding.progressBar.visibility = if (stores.isEmpty()) View.VISIBLE else View.GONE
         }
 
         mEditStoreViewModel = ViewModelProvider(this)[EditStoreViewModel::class.java]
