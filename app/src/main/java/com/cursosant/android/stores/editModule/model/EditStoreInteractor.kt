be8cd9bb1d1bd298846faa5924595938164944aa
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-11-30 00:30:39 -0500

    Impl() - Get Store by Id with LiveData

--- app/src/main/java/com/cursosant/android/stores/editModule/model/EditStoreInteractor.kt
+++ app/src/main/java/com/cursosant/android/stores/editModule/model/EditStoreInteractor.kt
@@ -1,11 +1,16 @@
 package com.cursosant.android.stores.editModule.model
 
+import androidx.lifecycle.LiveData
 import com.cursosant.android.stores.StoreApplication
 import com.cursosant.android.stores.common.entities.StoreEntity
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
 class EditStoreInteractor {
+
+    fun getStoreById(id: Long): LiveData<StoreEntity> {
+        return StoreApplication.database.storeDao().getStoreById(id)
+    }
 
     fun saveStore(storeEntity: StoreEntity, callback: (Long) -> Unit) {
         doAsync {
